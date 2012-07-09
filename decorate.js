376b3fae0e8b5beda991fd51f19ea32469993879
podgorniy <podgorniy.d@gmail.com>
2012-07-09 21:15:02 +0300

    comment

--- decorate.js
+++ decorate.js
@@ -15,12 +15,14 @@ function decorate (initial, decorate_before, decorate_after) {
 		var initial_call_result;
 
 		if (typeof decorate_before === 'function') {
-			if (!!decorate_before.apply(this, arguments) === false) return;
+			if (!!decorate_before.apply(this, arguments) === false) {
+				return;
+			}
 		}
 		initial_call_result = initial.apply(this, arguments);
 		if (typeof decorate_after === 'function') {
 			decorate_after.apply(this, arguments);
 		}
 		return initial_call_result;
-	}
+	};
 }
