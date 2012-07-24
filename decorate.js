9dea4c855456e307d354d916f018dea7b15420c1
podgorniy <podgorniy.d@gmail.com>
2012-07-24 21:33:17 +0300

    less safe realization with examples

--- decorate.js
+++ decorate.js
@@ -1,4 +1,22 @@
 /*
+	lite version
+	Ass function before or after another
+*/
+function decorate (initial, decorate_before, decorate_after) {
+	return function () {
+		var initial_call_result;
+
+		if (typeof decorate_before === 'function') {
+			decorate_before.apply(this, arguments);
+		}
+		initial_call_result = initial.apply(this, arguments);
+		if (typeof decorate_after === 'function') {
+			decorate_after.apply(this, arguments);
+		}
+		return initial_call_result;
+	};
+}
+/*
  Example of use
  document.write = decorate (document.write, function () {
 	console.log(this === document);// true;
@@ -10,19 +28,76 @@
 	console.log('After document.write');
  });
 */
+
+/*
+	1. add function before and after modified
+	2. change arguments, passed to modified
+	3. prevent modified from running
+	4. change modified returning value
+*/
 function decorate (initial, decorate_before, decorate_after) {
 	return function () {
-		var initial_call_result;
+		var initial_call_result,
+			updated_params,
+			updated_result;
 
 		if (typeof decorate_before === 'function') {
-			if (!!decorate_before.apply(this, arguments) === false) {
+			updated_params = decorate_before.apply(this, arguments);
+			if (!updated_params) {
 				return;
 			}
 		}
-		initial_call_result = initial.apply(this, arguments);
+		initial_call_result = initial.apply(this, updated_params || arguments);
 		if (typeof decorate_after === 'function') {
-			decorate_after.apply(this, arguments);
+			updated_result = decorate_after.apply(this, arguments);
 		}
-		return initial_call_result;
+		return updated_result || initial_call_result;
 	};
 }
+/*
+	example
+
+	// add before (logging calls before execution)
+	document.write = decorate(document.write, function () {
+		console.log('document.write called with', arguments);
+		return arguments;
+	});
+
+	// add after (logging call after execution)
+	document.write = decorate(document.write, null, function () {
+		console.log('after document.write');
+	});
+
+	// add before and after (profile calls)
+	document.write = decorate(document.write, function () {
+		console.profile('document.write');
+		return arguments;
+	}, function () {
+		console.profileEnd('document.write');
+	});
+
+	// prevent from executing
+	document.write = decorate(document.write, function () {
+		if (dom_is_ready()) {
+			return false;
+		} else {
+			return arguments;
+		}
+	});
+
+
+	// modify arguments
+	document.write = decorate(document.write, function (str) {
+		var args;
+
+		args = [replace_divs(str)];
+		return args;
+	}, document.write);
+
+	// change returning value
+	document.write = decorate(document.write, null, function () {
+		if (dom_is_ready()){
+			return true;
+		}
+	});
+*/
