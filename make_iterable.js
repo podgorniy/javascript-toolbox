1788a1147966ae30255653d3d70d5d9454539f09
podgorniy <podgorniy.d@gmail.com>
2012-08-06 20:53:19 +0300

    minor optimization

--- make_iterable.js
+++ make_iterable.js
@@ -1,11 +1,12 @@
-function make_arg_iterable (func) {
+function make_iterable (func) {
+	var arr_proto;
+
+	arr_proto = Array.prototype;
 	return function (el) {
 		var res,
 			args,
-			arr_proto,
 			i;
 
-		arr_proto = Array.prototype;
 		res = [];
 		args = [undefined].concat(arr_proto.slice.call(arguments, 1));
 		if ('length' in el) {
