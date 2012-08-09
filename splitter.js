b15572c841deee0412f69bf3d244b2b0f8215ad6
podgorniy <podgorniy.d@gmail.com>
2012-08-09 10:25:34 +0300

    Convesion of empty string case

--- splitter.js
+++ splitter.js
@@ -1,24 +1,28 @@
-function splitter (initial, pair_delimeter, key_value_delimeter) {
-	var pair_delimeter = pair_delimeter || '&',
-		key_value_delimeter = key_value_delimeter || '=',
-		param_type = Object.prototype.toString.call(initial),
+function splitter (initial, pair_separator, key_value_separator) {
+	var param_type = Object.prototype.toString.call(initial),
 		res,
 		kvPairs,
 		kvPair,
 		key;
 
+	pair_separator = pair_separator || '&';
+	key_value_separator = key_value_separator || '=';
 	if (param_type === '[object String]') {
 		res = {};
-		kvPairs = initial.split(pair_delimeter);
-		for (var i = 0; i < kvPairs.length; i += 1) {
-			kvPair = kvPairs[i].split(key_value_delimeter);
-			res[kvPair[0]] = kvPair[1];
+		if (initial) {
+			kvPairs = initial.split(pair_separator);
+			for (var i = 0; i < kvPairs.length; i += 1) {
+				kvPair = kvPairs[i].split(key_value_separator);
+				res[kvPair[0]] = kvPair[1];
+			}
 		}
 	} else if (param_type === '[object Object]') {
 		res = '';
 		for (key in initial) {
-			res ? res += (pair_delimeter + key + key_value_delimeter + initial[key])
-				: res = key + key_value_delimeter + initial[key];
+			if (initial.hasOwnProperty(key)) {
+				res ? res += (pair_separator + key + key_value_separator + initial[key])
+				: res = key + key_value_separator + initial[key];
+			}
 		}
 	}
 	return res;
