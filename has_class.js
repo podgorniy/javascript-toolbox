6b91d11353f8eea9d6c6a71332d7a50ef12df5dc
podgorniy <podgorniy.d@gmail.com>
2012-08-23 22:40:33 +0300

    Fix. Assignment without var

--- has_class.js
+++ has_class.js
@@ -1,11 +1,11 @@
-function has_class (node, class_names) {
-	var i;
+function has_class(node, class_names) {
+	var i, node_class;
 
 	class_names = class_names.replace(/^\s+|\s+$/g, '').split(/\s+/);
 	node_class = node.className && (' ' + node.className + ' ');
-	if ( node_class ) {
+	if (node_class) {
 		for (i = 0; i < class_names.length; i += 1) {
-			if ( node_class.indexOf(' ' + class_names[i] + ' ') === -1 ) {
+			if (node_class.indexOf(' ' + class_names[i] + ' ') === -1) {
 				return false;
 			}
 		}
