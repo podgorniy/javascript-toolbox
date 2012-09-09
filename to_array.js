4bf06e1d0f7f81a55a9cdbfb04c2132d95ea3bf2
podgorniy <podgorniy.d@gmail.com>
2012-09-09 14:31:13 +0300

    to_array
    Optimized for browsers, that support slice on array-like objects. Same
    speeds were shown for splice and for loop.
    http://jsperf.com/to-array

--- to_array.js
+++ to_array.js
@@ -1,3 +1,19 @@
+/*
+	Speed tests
+	http://jsperf.com/to-array
+*/
 function to_array (obj) {
-	return Object.prototype.toString.call(obj) !== '[object Array]' ? obj : Array.prototype.slice.call(obj);
+	var i,
+		length,
+		res;
+
+	if (Object.prototype.toString.call(obj) === '[object Array]') {
+		return obj;
+	}
+	length = obj.length;
+	res = [];
+	for (i = 0; i < length; i += 1) {
+		res.push(obj[i]);
+	}
+	return res;
 }
