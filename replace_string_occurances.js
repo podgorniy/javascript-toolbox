c8fcf75e247d777415edd1486359f9bc59c61124
podgorniy <podgorniy.d@gmail.com>
2012-11-27 14:06:50 +0200

    Escaped characters, that might be both symbols and part of a regexp

--- replace_string_occurances.js
+++ replace_string_occurances.js
@@ -2,7 +2,7 @@ function replace_string_occurances (src_string, string_to_replace, replace_with)
 	var escaped_string,
 		reg;
 
-	escaped_string = string_to_replace.replace(/[\(\)\[\]\\\.\^\$\|\?\+]/g, '\\$&');
+	escaped_string = string_to_replace.replace(/[\(\)\[\]\\\.\^\$\|\?\+\*\{\}]/g, '\\$&');
 	reg = new RegExp(escaped_string, 'g');
 	return src_string.replace(reg, replace_with);
 }
