4ae76b069e6f7a5748d8ec3891e6250d49a016c0
Podgorniy <podgorniy.d@gmail.com>
2013-03-26 16:14:37 +0200

    Remove spacing

--- worker.js
+++ worker.js
@@ -1,48 +1,48 @@
-	var worker = (function () {
-		var stack;
-		var callsGap;
-		var sinseLastCall;
-		var lastCall;
+var worker = (function () {
+	var stack;
+	var callsGap;
+	var sinseLastCall;
+	var lastCall;
 
-		// functions stack
-		stack = [];
+	// functions stack
+	stack = [];
 
-		// time between calls
-		callsGap = 999;
+	// time between calls
+	callsGap = 999;
 
-		// last time when function from stack was processed
-		lastCall = 0;
+	// last time when function from stack was processed
+	lastCall = 0;
 
-		// async iterator on functions stack
-		function processStack () {
-			var thisMoment;
+	// async iterator on functions stack
+	function processStack () {
+		var thisMoment;
 
-			thisMoment = now();
-			sinseLastCall = thisMoment - lastCall;
+		thisMoment = now();
+		sinseLastCall = thisMoment - lastCall;
 
-			// there are items to process and this function wasn't called 
-			// recentrly (less, than callsGap time)
-			if (sinseLastCall >= callsGap && stack.length) {
+		// there are items to process and this function wasn't called 
+		// recentrly (less, than callsGap time)
+		if (sinseLastCall >= callsGap && stack.length) {
 
-				// remember call time
-				lastCall = thisMoment;
+			// remember call time
+			lastCall = thisMoment;
 
-				// process first item
-				stack.shift()();
+			// process first item
+			stack.shift()();
 
-				// plan next process tick
-				// we plan it anyway, in case, new function will
-				// be added less, than in callsGap time 
-				setTimeout(processStack, callsGap);
-			}
+			// plan next process tick
+			// we plan it anyway, in case, new function will
+			// be added less, than in callsGap time 
+			setTimeout(processStack, callsGap);
 		}
+	}
 
-		function now () {
-			return new Date().getTime();
-		}
+	function now () {
+		return new Date().getTime();
+	}
 
-		return function (func) {
-			stack.push(func);
-			processStack();
-		};
-	}());
+	return function (func) {
+		stack.push(func);
+		processStack();
+	};
+}());
