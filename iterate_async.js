39b4dba871fbca406598aa70214d6a305ec5380c
Alexander <kemperomg@gmail.com>
2013-04-08 00:47:03 +0300

    Added final_arr like map
    log final array

--- iterate_async.js
+++ iterate_async.js
@@ -6,16 +6,22 @@ iterate_async(arr, function (el, index, arr) {
 }, 99);
 */
 function iterate_async (arr, callback, timeout) {
-	var index_of_processing_item;
+    var index_of_processing_item,
+        final_arr = [];
 
-	index_of_processing_item = 0;
-	(function proceed_next () {
-		if (index_of_processing_item < arr.length) {
-			setTimeout(function () {
-				callback.call(arr, arr[index_of_processing_item], index_of_processing_item, arr);
-				index_of_processing_item += 1;
-				proceed_next();
-			}, timeout || 50);
-		}
-	}());
+    index_of_processing_item = 0;
+    (function proceed_next () {
+        if (index_of_processing_item < arr.length) {
+            setTimeout(function () {
+                final_arr.push(callback.call(arr,
+                    arr[index_of_processing_item],
+                    index_of_processing_item,
+                    arr));
+                index_of_processing_item += 1;
+                proceed_next();
+            }, timeout || 50);
+        } else {
+            return console.log(final_arr);
+        }
+    }());
 }
